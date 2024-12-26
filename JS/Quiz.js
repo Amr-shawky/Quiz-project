a8c33e8ec90dea35d13d257f05521e6a8cacd39a
AMR-LORD <amrshawky936@gmail.com>
2024-12-27 01:15:27 +0200

    update on the buttons

--- JS/Quiz.js
+++ JS/Quiz.js
@@ -37,14 +37,21 @@
 
 
     // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
+    for (let button of optionButtons.children) {
+        button.addEventListener("click", (event) => { 
             // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
-
-
+            explanation.textContent = fact.explanation;
+            
             // TODO 7: Within the event handler function, 
             // Use a for loop to disable all the option buttons
-
-
+            for (let btn of optionButtons.children) {
+                disable(btn);
+            }
+            
+            
             // TODO 8: Within the event handler function,
             // Get the guessed value from the clicked button
             // Use a conditional to compare the guess to the fact's answer
             // and add the "correct"/"incorrect" class as appropriate
+        })
+    }
